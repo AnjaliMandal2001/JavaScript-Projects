8cecbbeeb0438d0aba197d7ff144c5ef024135d6
Anjali Mandal <mandalanjali2001@gmail.com>
2025-10-26 00:13:10 +0530

    intial

---
+++ practice/index.js
@@ -0,0 +1,21 @@
+//Normal function
+/*function print (){
+    console.log("normal function");
+}
+print()
+
+
+//Arrow function
+const print=()=>{
+    console.log("arrow function");
+}
+print()
+
+
+
+//Arrow function with arguments
+ const print=(a,b)=>{
+    console.log(a+b);
+}
+print(20,33)
+*/
